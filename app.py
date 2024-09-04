66592f32acf6abd7f5da0a357f713df31af5667f
akash017kumar@gmail.com <akash017kumar@gmail.com>
2024-09-04 08:28:49 -0400

    my first change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Be spiritual for Krishna"
+    return "RadhaAstami is on 11th sept, 2024"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
